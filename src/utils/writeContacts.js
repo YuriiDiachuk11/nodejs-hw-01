85e969c4b9fdb184dc4fb2ee3b4f3a56053efff7
YD <yuriidiachuk@icloud.com>
2025-01-08 23:28:30 +0200

    1,2 step done

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
-
-export const writeContacts = async (updatedContacts) => {};
+import fs from 'node:fs/promises';
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts);
+    await fs.writeFile(PATH_DB, data, 'utf8');
+    return updatedContacts;
+  } catch (error) {
+    console.error('Sorry, something go wrong way', error);
+    return null;
+  }
+};
