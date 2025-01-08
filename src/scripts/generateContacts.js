719a36fd183f451efaa7b915bac57050decc04a1
YD <yuriidiachuk@icloud.com>
2025-01-09 00:23:36 +0200

    3 step done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,23 @@
-const generateContacts = async (number) => {};
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'node:fs/promises';
+
+const generateContacts = async (number) => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+
+    const contacts = data ? JSON.parse(data) : [];
+
+    for (let i = 0; i < number; i++) {
+      const newContact = createFakeContact();
+      contacts.push(newContact);
+    }
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts), 'utf8');
+    console.log(`${number} new contacts added successfully!`);
+  } catch (error) {
+    console.error('Cannot add contacts, sorry', error);
+  }
+};
 
 generateContacts(5);
