cc85023faebadb1dccac57c9b5c1d646d3f94df1
YD <yuriidiachuk@icloud.com>
2025-01-09 11:47:51 +0200

    5 step done

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,13 @@
-export const getAllContacts = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+export const getAllContacts = async () => {
+  try {
+    const allContacts = await fs.readFile(PATH_DB, 'utf8');
+    return JSON.parse(allContacts);
+  } catch (error) {
+    console.error('Sorry, a problem happened', error);
+    return null;
+  }
+};
 
 console.log(await getAllContacts());
