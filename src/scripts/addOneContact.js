62711c5dc848d8fc2011ae8d7c647aa6e3b5a5cb
YD <yuriidiachuk@icloud.com>
2025-01-09 11:41:11 +0200

    4 step done

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+export const addOneContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+
+    fs.appendFile(PATH_DB, JSON.stringify(contacts), 'utf8');
+  } catch (error) {
+    console.error('Adding failed'), error;
+  }
+};
 
 addOneContact();
