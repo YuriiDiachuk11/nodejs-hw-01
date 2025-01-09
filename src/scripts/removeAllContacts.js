8ea31d4993af5e35d9c5d50c0f51ec233ef006ba
YD <yuriidiachuk@icloud.com>
2025-01-09 15:10:26 +0200

    add JSON.stringify

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 export const removeAllContacts = async () => {
   try {
-    await fs.writeFile(PATH_DB, [], 'utf8');
+    await fs.writeFile(PATH_DB, JSON.stringify([]), 'utf8');
   } catch (error) {
     console.error('Cannot delete', error);
   }
